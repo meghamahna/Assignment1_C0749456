ddd4215635a7697cfc933ca291a79b7f51776f2c
Megha Mahna <meghamahna04@gmail.com>
2020-01-14 21:46:11 -0500

    Finally Done!!!!!!

--- Assignment1_C0749456/ViewController.swift
+++ Assignment1_C0749456/ViewController.swift
@@ -9,24 +9,26 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+   
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var mapView: MKMapView!
     var temp = 0
-    
     var locationManager = CLLocationManager()
-    var user_latitude: CLLocationDegrees = 0.0
-    var user_longitude: CLLocationDegrees = 0.0
-    var new_latitude: CLLocationDegrees = 0.0
-    var new_longitude: CLLocationDegrees = 0.0
+    var destinationCoordinate: CLLocationCoordinate2D!
+    var annotation: MKPointAnnotation?
+    var mode_of_transport = 0;
+   // var newCoordinate: CLLocationCoordinate2D?
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+        //locationManager.startUpdatingLocation()
         mapView.showsUserLocation = true
         
         let dtgr = UITapGestureRecognizer(target: self, action: #selector(doubleTap))
@@ -39,19 +41,22 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     @objc func doubleTap(gestureRecognizer: UITapGestureRecognizer){
             
-            if temp == 0{
+//            for annotation in mapView.annotations{
+//                mapView.removeAnnotation(annotation)
+//            }
+//
+//            for overlay in mapView.overlays{
+//                mapView.removeOverlay(overlay)
+//            }
                 
                 let touchPoint = gestureRecognizer.location(in: mapView)
                 let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = coordinate
-                new_latitude = annotation.coordinate.latitude
-                new_longitude = annotation.coordinate.longitude
-                mapRoute(destinationCoordinate: annotation.coordinate)
-                temp = 1
+                mapView.addAnnotation(annotation)
+                destinationCoordinate = CLLocationCoordinate2D(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
                 
-                
-            }
+           
             
             
         }
@@ -104,8 +109,24 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let directionRequest = MKDirections.Request()
         directionRequest.source = sourceMapItem
         directionRequest.destination = destinationMapItem
-        directionRequest.transportType = .automobile
         
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+           
+            directionRequest.transportType = .automobile
+            mode_of_transport = 0
+        case 1:
+
+            directionRequest.transportType = .walking
+            mode_of_transport = 1
+        default:
+            
+            directionRequest.transportType = .automobile
+            mode_of_transport = 0
+            
+        }
+        
+    
         
         let directions = MKDirections(request: directionRequest)
         
@@ -129,14 +150,63 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
     }
     
+   
+    
+    
+    @IBAction func findMyWay(_ sender: UIButton) {
+        
+        
+        mapRoute(destinationCoordinate: destinationCoordinate)
+        
+    }
+    
+    
+    
+
+    @IBAction func zoomOut(_ sender: UIButton) {
+        
+        let value = mapView.userLocation.coordinate
+        let region = MKCoordinateRegion(center: value, latitudinalMeters: 850000*2, longitudinalMeters: 850000*2)
+        mapView.setRegion(region, animated: true)
+        
+    }
+}
+extension ViewController: MKMapViewDelegate{
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation{
+            
+            return nil
+        }
+            
+        else
+        {
+            let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
+            pin.animatesDrop = true
+            pin.tintColor = .red
+            return pin
+        }
+    }
+    
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if mode_of_transport == 0{
         let renderer = MKPolylineRenderer(overlay: overlay)
         renderer.strokeColor = UIColor.blue
         renderer.lineWidth = 4.0
-    
-        return renderer
+            return renderer
+        }
+        
+        else{
+            
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.red
+            renderer.lineWidth = 4.0
+            return renderer
+        }
+        return MKOverlayRenderer()
     }
-
-
 }
+
+
 
